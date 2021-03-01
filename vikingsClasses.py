61f97b5481e240cd6ea5a30c03aa2db8591bbb68
maria <alvarezferreiro.maria@gmail.com>
2021-03-01 21:36:43 +0100

    Soldiers

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,9 +1,41 @@
 
 # Soldier
+'''Modify the `Soldier` constructor function and add 2 methods to its prototype: `attack()`, and `receiveDamage()`.
 
+#### constructor function
+
+- should receive **2 arguments** (health & strength)
+- should receive the **`health` property** as its **1st argument**
+- should receive the **`strength` property** as its **2nd argument**
+
+#### `attack()` method
+
+- should be a function
+- should receive **0 arguments**
+- should return **the `strength` property of the `Soldier`**
+
+#### `receiveDamage()` method
+
+- should be a function
+- should receive **1 argument** (the damage)
+- should remove the received damage from the `health` property
+- **shouldn't return** anything
+
+---
+'''
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+
+
 
 # Viking
 
