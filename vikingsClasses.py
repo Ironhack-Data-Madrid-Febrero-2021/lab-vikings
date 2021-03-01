121f12da5f0acd4c28af7ac234eac8bbd4e04032
Ani <nitgarcia@hotmail.com>
2021-03-01 22:05:00 +0100

    A ver a ver

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,7 +3,14 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health  = health
+        self.strength = strength
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, the_damage):
+        self.health = {"the_damage"} - {"health"}
+        
 
 # Viking
 
