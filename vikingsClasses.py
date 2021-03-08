20992aac01ce5c40b64f6d5ae649850a14ea0dd1
Ani <nitgarcia@hotmail.com>
2021-03-08 22:45:57 +0100

    primeros test

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -9,13 +9,30 @@ class Soldier:
     def attack(self):
         return self.strength
     def receiveDamage(self, the_damage):
-        self.health = {"the_damage"} - {"health"}
+        self.health -= the_damage
         
 
 # Viking
 
 
-class Viking:
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, the_damage):
+        self.health -= the_damage
+        if self.health > self.the_damage:
+            print ("Viking", "{self.name}," "still alive has received", "{self.damage}" "points of damage") 
+        else:
+            print ("Viking dies" "{self.name}," "has died in act of combat")
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+    
+        
+    
     pass
 
 # Saxon
