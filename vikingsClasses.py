3dfa455c948d01c5c6382e5d916876b57b6f3864
maria <alvarezferreiro.maria@gmail.com>
2021-03-09 22:56:53 +0100

    9 para el bonus

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -49,19 +49,42 @@ class Saxon(Soldier):
 
 class War:
     def __init__(self):
-        vikingArmy = []
-        saxonArmy = []
+        self.vikingArmy = []
+        self.saxonArmy = []
 
     def addViking(self, Viking):
-        vikingArmy.append(Viking)
+        self.Viking = Viking
+        self.vikingArmy.append(Viking)
 
     def addSaxon(self,Saxon):
-        saxonArmy.append(Saxon)
+        self.Saxon = Saxon
+        self.saxonArmy.append(Saxon)
         
-    def vikingAttack():
-        pass
-    def saxonAttack():
-        pass
-    def showStatus():
-        pass
+    def vikingAttack(self):
+        import random
+        sa_ = random.choice(self.saxonArmy)
+        va_ = random.choice(self.vikingArmy)
+        Saxon.receiveDamage(Viking.strength) 
+        for a in saxonArmy:
+            if sa_ <= 0:
+                saxonArmy.pop(sa_)
+        return Saxon.receiveDamage(Viking.strength)
+
+    def saxonAttack(self):
+        import random
+        sv = random.choice(self.vikingArmy)
+        sa = random.choice(self.saxonArmy)
+        Viking.receiveDamage(Saxon.strength) 
+        for a in vikingArmy:
+            if sv_ <= 0:
+                vikingArmy.pop(sv)
+        return Viking.receiveDamage(Saxon.strength) 
+
+    def showStatus(self):
+        if saxonArmy == []:
+            return "Vikings have won the war of the century!"
+        elif vikingArmy == []:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(saxonArmy) >= 1 and len(vikingArmy) >= 1:
+            return "Vikings and Saxons are still in the thick of battle."
         
