27e0bfbce4cefeff75e5debd5afb248af1692e52
Pabloariza5 <pabloarizasuarez@gmail.com>
2021-03-10 12:20:11 +0100

    Lab-Vikings without war

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,69 @@
 
 
 class Soldier:
-    pass
+    def __init__ (self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        
 
 # Viking
 
+class Viking(Soldier):
+    def __init__ (self, name, health, strength):
+        self.name = name
+        super().__init__(health, strength)  
 
-class Viking:
-    pass
-
+    def attack(self):
+        return super().attack()
+    
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+    
+    def battleCry(self):
+        return "Odin Owns You All!"
+        
+   
 # Saxon
 
-
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__ (self, health, strength):
+        super().__init__(health, strength)
+    
+    def attack(self):
+        return super().attack()
+    
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"
+    
 
 # War
 
-
 class War:
-    pass
+    def __init__ (self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+        
+    def addViking(self, Viking):
+        self.vikingArmy.__add__(Viking)
+
+    def addSaxon(self, Saxon):
+        self.saxonArmy.__add__(Saxon)
+
+    def VikingAttack(self):
+        pass
+    def showStatus(self):
+        pass
