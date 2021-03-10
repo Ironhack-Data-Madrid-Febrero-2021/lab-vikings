d59a69b142560480f2015f53577f33fabd0d9776
Honaida <correohonaida@gmail.com>
2021-03-10 16:49:26 +0100

    lab vikings OOP

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,24 +1,49 @@
+import numpy as np
 
 # Soldier
 
 
 class Soldier:
-    pass
+    def __init__ (self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health -= self.damage
 
 # Viking
 
-
-class Viking:
+class Viking(Soldier):
+    def __init__ (self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return (f"{self.name} has received {damage} points of damage")
+        else:
+            return (f"{self.name} has died in act of combat")
+    def battleCry(self):
+        return "Odin Owns You All!"
     pass
 
 # Saxon
 
+class Saxon(Soldier):
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return (f"A Saxon has received {damage} points of damage")
+        else:
+            return (f"A Saxon has died in combat")
 
-class Saxon:
     pass
 
 # War
-
 
 class War:
     pass
